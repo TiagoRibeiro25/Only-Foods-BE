261cfc9f1bb340a879a3c831c8bd823858edc914
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-07-04 15:08:16 +0100

    refactor: Moved valid envs to a json file

--- src/utils/checkEnvs.ts
+++ src/utils/checkEnvs.ts
@@ -1,18 +1,5 @@
 import colors from 'colors';
-const VALID_ENVS = [
-	'NODE_ENV',
-	'PORT',
-	'ENABLE_LOGGING',
-	'JWT_SECRET',
-	'JWT_EXPIRES_IN',
-	'JWT_EXPIRES_IN_REMEMBER_ME',
-	'JWT_GENERATE_TOKEN_IN',
-	'MAILJET_URL',
-	'MAILJET_PUBLIC_KEY',
-	'MAILJET_SECRET_KEY',
-	'MAILJET_FROM_EMAIL',
-	'DATABASE_URL',
-];
+import VALID_ENVS from '../data/envs.json';
 
 /**
  * Check if all the required environment variables are set
