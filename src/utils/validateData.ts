39708528b6b94a50ea0c68954dbdcc9479fc48bd
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-07-11 21:27:52 +0100

    fix: Fixed validate description
    Added missing ":" character

--- src/utils/validateData.ts
+++ src/utils/validateData.ts
@@ -33,7 +33,6 @@ const username = (usernameInput: string): boolean => {
 	return isValidFormat && isValidLength;
 };
 
-//TODO: Fix punctuation marks validation
 /**
  * Validate a description
  * - Can have letters, numbers, spaces, and common punctuation marks
@@ -46,7 +45,7 @@ const description = (descriptionInput: string): boolean => {
 		return false;
 	}
 
-	const descriptionRegex = /^[a-zA-Z0-9\s,.!?'"\\-]+$/;
+	const descriptionRegex = /^[a-zA-Z0-9\s:,.!?'"\\-]+$/;
 	const isValidFormat = descriptionRegex.test(descriptionInput);
 	const isValidLength = descriptionInput.length >= 10 && descriptionInput.length <= 200;
 	return isValidFormat && isValidLength;
