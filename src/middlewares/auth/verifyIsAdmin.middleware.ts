b7ae6b322f20c26fd062dbbf6e8838a5aa53e3d0
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-07-11 15:32:18 +0100

    feat: Added middleware to check if the user is admin

--- src/middlewares/auth/verifyIsAdmin.middleware.ts
+++ src/middlewares/auth/verifyIsAdmin.middleware.ts
@@ -1,10 +1,27 @@
 import { NextFunction, Response } from 'express';
 import { Request } from 'types';
+import prisma from '../../config/db.config';
+import handleError from '../../utils/handleError';
 
 export default async (req: Request, res: Response, next: NextFunction): Promise<void> => {
 	// Get logged user id
 	const userId = req.tokenData.id;
 
-	// try {
-	// } catch (error) {}
+	try {
+		// Get the user from the database
+		const user = await prisma.user.findUnique({
+			where: { id: userId },
+			select: { isAdmin: true },
+		});
+
+		// If the user is not an admin, throw an error
+		if (!user.isAdmin) {
+			throw new Error('Insufficient permissions');
+		}
+
+		// If the user is an admin, call the next middleware
+		next();
+	} catch (error) {
+		handleError({ res, error });
+	}
 };
