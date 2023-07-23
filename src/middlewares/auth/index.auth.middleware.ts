bb11d3fbd5b482051a335670c5fb0c1ccb72bc9e
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-07-23 22:36:19 +0100

    revert: Removed useless import

--- src/middlewares/auth/index.auth.middleware.ts
+++ src/middlewares/auth/index.auth.middleware.ts
@@ -1,13 +1,6 @@
 import handleToken from './handleToken.middleware';
-import updateTokenWhiteList from './updateTokenWhiteList.middleware';
 import verifyIfBlocked from './verifyIfBlocked.middleware';
 import verifyIsAdmin from './verifyIsAdmin.middleware';
 import verifyToken from './verifyToken.middleware';
 
-export default {
-	handleToken,
-	verifyToken,
-	verifyIsAdmin,
-	verifyIfBlocked,
-	updateTokenWhiteList,
-};
+export default { handleToken, verifyToken, verifyIsAdmin, verifyIfBlocked };
