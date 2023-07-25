00713d1318a4ee50335fd3dd04dc22577224f338
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-07-25 23:37:44 +0100

    feat: Added an actual email template
    Added a template for the email sent for resetting an account password.
    
    Instead of being an email with just the token to reset the password, now the email has some friendly text and a funcional link to update/reset the account password

--- src/controllers/users/forgotPassword.controller.ts
+++ src/controllers/users/forgotPassword.controller.ts
@@ -2,16 +2,15 @@ import { Response } from 'express';
 import { Request } from 'types';
 import prisma from '../../config/db.config';
 import sendEmail from '../../services/sendEmail';
+import resetPasswordEmail from '../../templates/resetPasswordEmail';
 import handleError from '../../utils/handleError';
 import handleResponse from '../../utils/handleResponse';
 
 export default async (req: Request, res: Response): Promise<void> => {
 	try {
-		const email: string = req.body.email;
-
 		// Check there's an account with the email provided
 		const user = await prisma.user.findUnique({
-			where: { email },
+			where: { email: req.body.email },
 		});
 
 		if (!user) {
@@ -19,13 +18,16 @@ export default async (req: Request, res: Response): Promise<void> => {
 		}
 
 		const token: string = user.resetPasswordToken;
+		const email: string = resetPasswordEmail({
+			username: user.username,
+			resetPasswordUrl: `${process.env.FRONTEND_URL}/reset-password/${token}`,
+		});
 
-		// TODO: Update the email content with the email template and correct link
 		await sendEmail({
 			from: 'Only Foods Support Team',
 			to: [{ email: user.email, name: user.username }],
 			subject: 'Reset your password',
-			content: `Reset password token: ${token}`,
+			content: email,
 		});
 
 		handleResponse({
