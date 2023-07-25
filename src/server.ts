9031f6c7cb4d2d66395838068341258f105fae3b
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-07-25 19:29:45 +0100

    refactor: Log more info when starting the server

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,26 @@
 import colors from 'colors';
 import checkEnvs from './utils/checkEnvs';
 
+// Clear the console
+console.clear();
+
+// Log the environment
+console.log(
+	colors.yellow('[server.ts] ') +
+		colors.cyan('Environment: ') +
+		colors.yellow(process.env.NODE_ENV),
+);
+
+// Load the environment variables from the .env file (development only)
 if (process.env.NODE_ENV !== 'production') {
 	// eslint-disable-next-line @typescript-eslint/no-var-requires
 	require('dotenv').config();
 }
+
+// Check if all the environment variables are set
+console.log(
+	colors.yellow('[server.ts] ') + colors.cyan('Checking the environment variables...'),
+);
 
 if (!checkEnvs()) {
 	process.exit(1);
@@ -15,8 +31,10 @@ import connectToDatabases from './services/connectToDatabases';
 const PORT: string = process.env.PORT;
 
 app.listen(PORT, () => {
-	console.clear();
 	console.log(colors.yellow('[server.ts] ') + colors.cyan('Starting the server...'));
+	console.log(
+		colors.yellow('[server.ts] ') + colors.cyan('Connecting to the databases...'),
+	);
 
 	// Connect to the databases
 	connectToDatabases()
@@ -24,7 +42,7 @@ app.listen(PORT, () => {
 			console.log(
 				colors.yellow('[server.ts] ') +
 					colors.cyan('Server is running on port: ') +
-					colors.yellow(PORT),
+					colors.yellow(PORT + '\n'),
 			);
 		})
 		.catch(error => {
