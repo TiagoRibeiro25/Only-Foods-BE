3212a06b073d1e87015ef47364b8133906e506fe
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-07-29 19:02:08 +0100

    refactor: Delete token from redis and mongodb on logout

--- src/controllers/users/logout.controller.ts
+++ src/controllers/users/logout.controller.ts
@@ -1,12 +1,21 @@
 import { Response } from 'express';
+import mongodb from '../../config/mongo.config';
+import redis from '../../config/redis.config';
 import { Request } from '../../types';
 import handleError from '../../utils/handleError';
 import handleResponse from '../../utils/handleResponse';
 
-export default (req: Request, res: Response): void => {
+export default async (req: Request, res: Response): Promise<void> => {
 	try {
-		// Delete the cookie authorization (req.cookies['authorization'])
+		// Get the cookie authorization (req.cookies['authorization'])
+		const cookie = req.cookies['authorization'];
+
+		// Delete the cookie authorization
 		res.clearCookie('authorization');
+
+		// Delete the token from the Redis cache and MongoDB collection
+		await redis.del(cookie);
+		await mongodb.db.collection('tokens').deleteOne({ token: cookie });
 
 		// Send the response
 		handleResponse({
