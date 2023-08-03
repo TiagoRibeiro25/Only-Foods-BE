48e4e68336a541df7dca421f21e504695774a934
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-08-03 14:04:33 +0100

    perf: Check if the user is an admin from the token

--- src/middlewares/auth/verifyIsAdmin.middleware.ts
+++ src/middlewares/auth/verifyIsAdmin.middleware.ts
@@ -1,25 +1,11 @@
 import { NextFunction, Response } from 'express';
-import prisma from '../../config/db.config';
 import { Request } from '../../types';
 import handleError from '../../utils/handleError';
 
-interface User {
-	isAdmin: boolean;
-}
-
-export default async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-	// Get logged user id
-	const userId: number = req.tokenData.id;
-
+export default (req: Request, res: Response, next: NextFunction): void => {
 	try {
-		// Get the user from the database
-		const user: User = await prisma.user.findUnique({
-			where: { id: userId },
-			select: { isAdmin: true },
-		});
-
 		// If the user is not an admin, throw an error
-		if (!user.isAdmin) {
+		if (req.tokenData.isAdmin) {
 			throw new Error('Insufficient permissions');
 		}
 
