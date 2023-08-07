685809dad5c7c5c93f18d83a5070a82f16dfab6a
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-08-07 17:36:16 +0100

    test: Updated validateData tests

--- __tests__/utils/validateData.test.ts
+++ __tests__/utils/validateData.test.ts
@@ -122,3 +122,149 @@ describe('id', () => {
 		});
 	});
 });
+
+describe('recipeTitle', () => {
+	it('should return true for a valid recipe title', () => {
+		const validTitles = [
+			'Delicious Recipe',
+			'Recipe with numbers: 123',
+			'Recipe with punctuation: !?.,',
+		];
+
+		validTitles.forEach(title => {
+			const isValid = validateUtils.recipeTitle(title);
+			expect(isValid).toBe(true);
+		});
+	});
+
+	it('should return false for an invalid recipe title', () => {
+		const invalidTitles = [
+			'',
+			'A',
+			'A very long recipe title that exceeds the maximum limit of 50 characters.',
+		];
+
+		invalidTitles.forEach(title => {
+			const isValid = validateUtils.recipeTitle(title);
+			expect(isValid).toBe(false);
+		});
+	});
+});
+
+describe('pagination', () => {
+	it('should return true for valid pagination data', () => {
+		const validData = [
+			{ page: 1, limit: 10 },
+			{ page: 2, limit: 20 },
+			{ page: '3', limit: '30' },
+		];
+
+		validData.forEach(data => {
+			const isValid = validateUtils.pagination(data);
+			expect(isValid).toBe(true);
+		});
+	});
+
+	it('should return false for invalid pagination data', () => {
+		const invalidData = [
+			{ page: 0, limit: 10 },
+			{ page: 2, limit: 0 },
+			{ page: -3, limit: 30 },
+			{ page: 'invalid', limit: '30' },
+		];
+
+		invalidData.forEach(data => {
+			const isValid = validateUtils.pagination(data);
+			expect(isValid).toBe(false);
+		});
+	});
+});
+
+describe('thoughtContent', () => {
+	it('should return true for valid thought content', () => {
+		const validThoughts = [
+			'This is a valid thought content.',
+			'A valid thought with punctuation marks: !?.,',
+			'A valid thought with numbers: 1234567890',
+		];
+
+		validThoughts.forEach(thought => {
+			const isValid = validateUtils.thoughtContent(thought);
+			expect(isValid).toBe(true);
+		});
+	});
+
+	it('should return false for invalid thought content', () => {
+		const invalidThoughts = [
+			'', // Empty string
+			'A very long thought that exceeds the maximum limit of 1000 characters. '.repeat(
+				20,
+			), // Too long
+		];
+
+		invalidThoughts.forEach(thought => {
+			const isValid = validateUtils.thoughtContent(thought);
+			expect(isValid).toBe(false);
+		});
+	});
+});
+
+describe('recipeNotes', () => {
+	it('should return true for valid recipe notes', () => {
+		const validNotes = [
+			'This is a valid recipe note.',
+			'A valid note with punctuation marks: !?.,',
+			'A valid note with numbers: 1234567890',
+		];
+
+		validNotes.forEach(note => {
+			const isValid = validateUtils.recipeNotes(note);
+			expect(isValid).toBe(true);
+		});
+	});
+
+	it('should return false for invalid recipe notes', () => {
+		const invalidNotes = [
+			'', // Empty string
+			'A', // Too short
+			'A very long recipe note that exceeds the maximum limit of 2000 characters. '.repeat(
+				30,
+			), // Too long
+		];
+
+		invalidNotes.forEach(note => {
+			const isValid = validateUtils.recipeNotes(note);
+			expect(isValid).toBe(false);
+		});
+	});
+});
+
+describe('commentContent', () => {
+	it('should return true for valid comment content', () => {
+		const validComments = [
+			'This is a valid comment.',
+			'A valid comment with punctuation marks: !?.,',
+			'A valid comment with numbers: 1234567890',
+		];
+
+		validComments.forEach(comment => {
+			const isValid = validateUtils.commentContent(comment);
+			expect(isValid).toBe(true);
+		});
+	});
+
+	it('should return false for invalid comment content', () => {
+		const invalidComments = [
+			'', // Empty string
+			'A', // Too short
+			'A very long comment that exceeds the maximum limit of 2000 characters. '.repeat(
+				30,
+			), // Too long
+		];
+
+		invalidComments.forEach(comment => {
+			const isValid = validateUtils.commentContent(comment);
+			expect(isValid).toBe(false);
+		});
+	});
+});
