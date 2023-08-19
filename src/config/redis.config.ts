05aa7d09a51c48b163918ee7559486b4d4cf47fc
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-08-19 22:28:08 +0100

    refactor: Added a timeout to the redis connection
    If the connection fails 5 times when trying connecting to Redis, throw an error to stop the server from running.

--- src/config/redis.config.ts
+++ src/config/redis.config.ts
@@ -3,7 +3,20 @@ import Redis from 'ioredis';
 // Connection URL
 const url: string = process.env.REDIS_URL;
 
-// Connect to Redis
-const redis = new Redis(url);
+// Configure connection options with a timeout
+const connectionOptions = {
+	retryStrategy: (times: number) => {
+		// Retry for a maximum of 5 times
+		if (times >= 5) {
+			// Throw an error after the maximum retries
+			throw new Error('Failed to connect to Redis after multiple attempts');
+		}
+		// Retry after 1 second
+		return 1000;
+	},
+};
+
+// Connect to Redis with the specified options
+const redis = new Redis(url, connectionOptions);
 
 export default redis;
