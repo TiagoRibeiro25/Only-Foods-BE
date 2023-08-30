2c136a6807b18682468f8937594590a0613c0187
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-08-30 15:11:15 +0100

    refactor: Replaced hardcoded redis db index with env

--- src/config/redis.config.ts
+++ src/config/redis.config.ts
@@ -17,7 +17,7 @@ const connectionOptions = {
 const redis = new Redis({
 	host: process.env.REDIS_HOST,
 	port: parseInt(process.env.REDIS_PORT),
-	db: 0,
+	db: parseInt(process.env.REDIS_DB),
 	...connectionOptions,
 });
 
