efd2752fee133d3d37d5a0837d4b76500972ff23
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2025-03-28 15:25:55 +0000

    Added missing Redis credentials

--- src/config/redis.config.ts
+++ src/config/redis.config.ts
@@ -18,6 +18,8 @@ const redis = new Redis({
 	host: process.env.REDIS_HOST,
 	port: parseInt(process.env.REDIS_PORT),
 	db: parseInt(process.env.REDIS_DB),
+	username: process.env.REDIS_USERNAME,
+	password: process.env.REDIS_PASSWORD,
 	...connectionOptions,
 });
 
