fccee97ed98435c07d16322b47829be39e611352
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2026-01-03 21:32:44 +0000

    Updated redis connection

--- src/config/redis.config.ts
+++ src/config/redis.config.ts
@@ -1,26 +1,28 @@
 import Redis from 'ioredis';
 
 // Configure connection options with a timeout
-const connectionOptions = {
-	retryStrategy: (times: number) => {
-		// Retry for a maximum of 5 times
-		if (times >= 5) {
-			// Throw an error after the maximum retries
-			throw new Error('Failed to connect to Redis after multiple attempts');
-		}
-		// Retry after 1 second
-		return 1000;
-	},
-};
+// const connectionOptions = {
+// 	retryStrategy: (times: number) => {
+// 		// Retry for a maximum of 5 times
+// 		if (times >= 5) {
+// 			// Throw an error after the maximum retries
+// 			throw new Error('Failed to connect to Redis after multiple attempts');
+// 		}
+// 		// Retry after 1 second
+// 		return 1000;
+// 	},
+// };
 
-// Connect to Redis with the specified options
-const redis = new Redis({
-	host: process.env.REDIS_HOST,
-	port: parseInt(process.env.REDIS_PORT),
-	db: parseInt(process.env.REDIS_DB),
-	username: process.env.REDIS_USERNAME,
-	password: process.env.REDIS_PASSWORD,
-	...connectionOptions,
-});
+// // Connect to Redis with the specified options
+// const redis = new Redis({
+// 	host: process.env.REDIS_HOST,
+// 	port: parseInt(process.env.REDIS_PORT),
+// 	db: parseInt(process.env.REDIS_DB),
+// 	username: process.env.REDIS_USERNAME,
+// 	password: process.env.REDIS_PASSWORD,
+// 	...connectionOptions,
+// });
+
+const redis = new Redis(process.env.REDIS_URL);
 
 export default redis;
